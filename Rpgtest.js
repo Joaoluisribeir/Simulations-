e8307b2458aaf7342701f237e2cdb05316a38499
Joaoluisribeir <144291980+Joaoluisribeir@users.noreply.github.com>
2026-07-24 22:51:16 -0300

    Implement Npc and Play classes
    Added Npc and Play classes with constructors and methods.

---
+++ Rpgtest.js
@@ -0,0 +1,29 @@
+class Npc{
+  constructor(name,age,profession){
+    this.name = name;
+    this.age = age;
+    this.profession = profession;
+  }
+  topresent(name){
+    name = this.name;
+    console.log(`hi my name is ${name}`);
+  }
+  }
+
+class Play extends Npc{
+  constructor(name, age,profession,level){
+    super(name,age,profession);
+    this.leve = level;
+  }
+}
+
+
+let any = new Npc('any', 22, 'farmer');
+let leo = new Npc('leo', 25, 'gate guard');
+let misa = new Npc('misa', 20, 'adventurous');
+
+let play = new Play('jonh', 20, 'warrior', 10);
+console.log(play);
+any.topresent()
+leo.topresent();
+misa.topresent();
